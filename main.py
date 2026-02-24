d8ef0a20c107c197391f6b8c05e3079396d1f7c8
Sage <ethanjameslong1@gmail.com>
2026-02-24 18:25:29 -0500

    Added spotify auto-launch and auto play when closed, needs to be optimized

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+import pyperclip
 import io
 import proc
 import sys
@@ -25,11 +27,34 @@ while c != "q":
     match c:
         case "a":
             subprocess.run("hns")
+            filename = "./transcriptions.txt"
+            try:
+                clipboard_content = pyperclip.paste()
+                if not clipboard_content:
+                    print(
+                        "Clipboard is empty or contains non-text data (e.g., an image). No file created."
+                    )
+                    break
+                with open(filename, "w") as f:
+                    f.write(clipboard_content)
+                print(f"Successfully wrote clipboard content to {filename}")
+
+            except pyperclip.PyperclipException as e:
+                print(f"Error accessing clipboard: {e}")
+            except IOError as e:
+                print(f"Error writing to file: {e}")
         case "s":
             foundFlag = False
-            output = subprocess.run(["playerctl", "--list-all"])
-            proc = subprocess.Popen(["prog", "arg"], stdout=subprocess.PIPE)
+            proc = subprocess.Popen(["playerctl", "--list-all"], stdout=subprocess.PIPE)
             for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
                 if line == "Spotify":
                     subprocess.run(["playerctl", "--player=spotify", "play"])
                     foundFlag = True
+            if not foundFlag:
+                subprocess.Popen(
+                    ["spotify", "--minimized"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+                time.sleep(3)
+                subprocess.run(["playerctl", "--player=spotify", "play"])
