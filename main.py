8dfc5c7014c88453b4714d20a1ea0836ab138e39
Sage <ethanjameslong1@gmail.com>
2026-02-25 19:02:38 -0500

    Tried and failed to get offline playlist choosing support, need to get playlist URI's

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 import termios
 import tty
 
+RAP_URI = "https://open.spotify.com/playlist/1meh7fDcfNbjZrOz3jIlQU?si=e583a37e9cc94f81"
+
 
 def getch():
     fd = sys.stdin.fileno()
@@ -30,6 +32,7 @@ while c != "q":
             filename = "./transcriptions.txt"
             try:
                 clipboard_content = pyperclip.paste()
+                clipboard_content += "\n###"
                 if not clipboard_content:
                     print(
                         "Clipboard is empty or contains non-text data (e.g., an image). No file created."
@@ -58,3 +61,18 @@ while c != "q":
                 )
                 time.sleep(3)
                 subprocess.run(["playerctl", "--player=spotify", "play"])
+        case "r":
+            foundFlag = False
+            proc = subprocess.Popen(["playerctl", "--list-all"], stdout=subprocess.PIPE)
+            for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):
+                if line == "Spotify":
+                    subprocess.run(["playerctl", "--player=spotify", "play"])
+                    foundFlag = True
+            if not foundFlag:
+                subprocess.Popen(
+                    ["spotify", "--minimized"],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+                time.sleep(3)
+            subprocess.run(["playerctl", "open", RAP_URI])
